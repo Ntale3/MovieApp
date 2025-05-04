baae2f4aaa84fb9234a05c6b6572c72ed58f9442
Ntale3 <swamaduntale98@gmail.com>
2025-05-04 17:13:00 +0300

    complete tabBar with navigation functionality

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,31 +1,88 @@
-import { Image, ImageBackground,Text} from 'react-native'
+import { Image, ImageBackground,Text, View} from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
 import { images } from '@/constants/images'
+import '../global.css'
+import { icons } from '@/constants/icons'
+
+// interface props{
+//   focused:boolean,
+//   icon:string,
+//   title:string
+// }
+
+
+const TabIcon=({focused,icon,title})=>{
+if(focused){
+  return (<ImageBackground
+  source={images.highlight}
+  className='flex flex-row w-full min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden'
+    >
+      <Image
+      source={icon}
+      tintColor="#151312" className='size-5'
+
+      />
+  <Text className='text-secondary text-base font-semibold ml-2'>{title}</Text>
+    </ImageBackground>
+   
+)}
+
+return(
+  <View className='size-full justify-center items-center mt-4 rounded-full'>
+    <Image
+    source={icon}
+    tintColor="#A8B5DB"
+    className='size-5'
+    />
+  </View>
+)
+}
+
+
 
 const _Layout = () => {
   return (
-   <Tabs>
+   <Tabs
+   screenOptions={{
+    tabBarShowLabel:false,
+    tabBarItemStyle:{
+      width: '100%',
+      height: '100%',
+      justifyContent:'center',
+      alignItems:'center'
+    },
+    tabBarStyle:{
+      backgroundColor:'#0f0D23',
+      borderRadius: 50,
+      marginHorizontal:20,
+      marginBottom:36,
+      position:'absolute',
+      overflow:'hidden',
+      borderWidth: 1,
+      borderColor:'#0f0D23'
+    }
+
+   }}
+   
+   
+   
+   
+   >
 <Tabs.Screen
 name="index"
 options={
     {
         title:"Home",
         headerShown:false,
-       tabBarIcon: ({focused})=>{
+       tabBarIcon: ({focused})=>(
           <>
-          <ImageBackground
-        source={images.highlight}
-        className='flex flex-row w-full flex-1 min-w-[112px]'
-          >
-        <Image
-source={images.home}
-tintColor="#151312" className='size-5'
-        />
-
-          </ImageBackground>
+         <TabIcon focused={focused} 
+                  icon={icons.home} 
+                  title="Home"/>
+         
           </>
-       }
+       )
     }
 }
 
@@ -36,7 +93,15 @@ name="search"
 options={
     {
         title:"Search",
-        headerShown:false
+        headerShown:false,
+        tabBarIcon: ({focused})=>(
+          <>
+         <TabIcon focused={focused} 
+                  icon={icons.search} 
+                  title="Search"/>
+         
+          </>
+       )
     }
 }
 
@@ -48,7 +113,15 @@ name="save"
 options={
     {
         title:"Save",
-        headerShown:false
+        headerShown:false,
+        tabBarIcon: ({focused})=>(
+          <>
+         <TabIcon focused={focused} 
+                  icon={icons.save} 
+                  title="Saved"/>
+         
+          </>
+       )
     }
 }
 
@@ -58,7 +131,15 @@ name="profile"
 options={
     {
         title:"Profile",
-        headerShown:false
+        headerShown:false,
+        tabBarIcon: ({focused})=>(
+          <>
+         <TabIcon focused={focused} 
+                  icon={icons.person} 
+                  title="Profile"/>
+         
+          </>
+       )
     }
 }
 
