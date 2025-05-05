64ebd12762a01c116c073139d6f34a363fe7c023
Ntale3 <swamaduntale98@gmail.com>
2025-05-05 17:47:55 +0300

    set up the tmdb api and the created a custom hook (useEffect in the service folder to help ease fetch in tmdb url

---
+++ services/api.ts
@@ -0,0 +1,48 @@
+
+
+export const TMDB_CONFIG={
+    BASE_URL:'https://api.themoviedb.org/3/',
+    API_KEY:process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+    header:{
+        accept:'application/json',
+        Authorization:`Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
+    }
+}
+
+const fetchPopularMovies=async({query}:{query:string})=>{
+    const endpoint=query?`${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`    
+                        : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`
+
+    const response=await fetch(endpoint,{
+        method:'GET',
+        headers:TMDB_CONFIG.header
+    });
+    if(!response.ok){
+        //@ts-ignore
+        throw new Error("Failed to fetch movies",response.statusText);
+        
+    }
+    const data=await response.json();
+
+    return data.results
+
+}
+export default fetchPopularMovies;
+
+
+
+
+
+// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOTE3NDUzN2ZhN2VjMmFhNDViZTc5MjFjMzAzOTNkMyIsIm5iZiI6MTc0NjQ0NjAzMS40NzksInN1YiI6IjY4MThhNmNmZTcwMTNjZWMwODQ4NzgyNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XSQWAeT6v24dMgqFGG1nZfeQTcaPdOPGHb0lO0kmV1g'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error(err));
